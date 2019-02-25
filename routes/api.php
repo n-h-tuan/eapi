7ea7f00e45c53e0948af1209a32f59e16bfa2a8f
Tuan Nguyen Hoang <nguyenledly@gmail.com>
2019-02-25 13:14:15 +0700

    Make Model -a

--- routes/api.php
+++ routes/api.php
@@ -44,8 +44,14 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 //     return 204;
 // });
 
-Route::get('articles','ArticleController@index');
-Route::get('articles/{article}','ArticleController@show');
-Route::post('articles','ArticleController@store');
-Route::put('articles/{article}','ArticleController@update');
-Route::delete('articles/{article}','ArticleController@delete');
+// Route::get('articles','ArticleController@index');
+// Route::get('articles/{article}','ArticleController@show');
+// Route::post('articles','ArticleController@store');
+// Route::put('articles/{article}','ArticleController@update');
+// Route::delete('articles/{article}','ArticleController@delete');
+
+Route::apiResource('/product','ProductController');
+
+Route::group(['prefix' => 'product'], function () {
+    Route::apiResource('/{product}/review', 'ReviewController');
+});
