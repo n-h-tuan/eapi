98bbdfa66227cd0b4fe62e2f307f44c8f92bf470
Tuan Nguyen Hoang <nguyenledly@gmail.com>
2019-02-25 20:50:11 +0700

    Product Resource created

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Model\Product;
 use Illuminate\Http\Request;
+use App\Http\Resources\Product\ProductResource;
 
 class ProductController extends Controller
 {
@@ -14,7 +15,7 @@ class ProductController extends Controller
      */
     public function index()
     {
-        //
+        return Product::all();
     }
 
     /**
@@ -31,7 +32,7 @@ class ProductController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        
     }
 
     /**
@@ -42,7 +43,8 @@ class ProductController extends Controller
      */
     public function show(Product $product)
     {
-        //
+        // return new ProductResource($product);
+        return $product;
     }
 
     /**
